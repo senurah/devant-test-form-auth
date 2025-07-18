abf739573bea9ed03ba42d26beb081c55f89e68d
senurah <neophytetoskill@gmail.com>
2025-07-18 18:59:44 +0530

    Update server port and redirect URLs to 3001 for consistency

--- app.js
+++ app.js
@@ -15,6 +15,6 @@ const server = http.createServer((req, res) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+server.listen(3001, () => {
+  console.log("Server running at http://localhost:3001");
 });
